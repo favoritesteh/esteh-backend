4f519520082e7a1809e03466fa1eda901884ef32
favoritesteh <favoritesteh@gmail.com>
2025-12-03 10:01:10 +0700

    perbaikan User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,21 +5,53 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
-    // HAPUS 'password' DARI SINI!
-    protected $fillable = ['username', 'role', 'outlet_id'];
+    /**
+     * Field yang boleh diisi via mass assignment
+     */
+    protected $fillable = [
+        'username',
+        'password',
+        'role',
+        'outlet_id'
+    ];
 
-    protected $hidden = ['password'];
+    /**
+     * Field yang disembunyikan saat return JSON
+     */
+    protected $hidden = [
+        'password'
+    ];
 
+    /**
+     * Hash password otomatis
+     */
+    public function setPasswordAttribute($value)
+    {
+        // Kalau password sudah ter-hash, jangan hash ulang
+        if (strlen($value) === 60 && password_get_info($value)['algo']) {
+            $this->attributes['password'] = $value;
+        } else {
+            $this->attributes['password'] = Hash::make($value);
+        }
+    }
+
+    /**
+     * Relasi: User → Outlet
+     */
     public function outlet()
     {
         return $this->belongsTo(Outlet::class);
     }
 
+    /**
+     * Relasi: Karyawan → Transaksi
+     */
     public function transaksi()
     {
         return $this->hasMany(Transaksi::class, 'karyawan_id');
