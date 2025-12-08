ce84f27ff65ab9ac977f246b1008d71b4da92ca8
favoritesteh <favoritesteh@gmail.com>
2025-12-08 14:45:17 +0700

    fix trust proxies for railway

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,7 +9,7 @@ class TrustProxies extends Middleware
 {
     protected $proxies = '*'; // <— INI WAJIB DI RAILWAY
 
-    protected $headers = 
+    protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
