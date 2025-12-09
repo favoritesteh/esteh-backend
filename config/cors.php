4475de4ddb2dc05f43691be65045400d7c1ebf3f
favoritesteh <favoritesteh@gmail.com>
2025-12-09 12:44:53 +0700

    lagi yah

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,12 @@
 <?php
 
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', '*'],  // FIX UTAMA
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://esteh-backend-production.up.railway.app',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:8081',
-        'http://127.0.0.1:8081',
+        '*',  // Kalau pakai Railway lebih aman buka dulu
     ],
 
     'allowed_origins_patterns' => [],
