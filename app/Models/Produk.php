c63af128b4df5b909b776150c67342ca3dbc2a15
favoritesteh <favoritesteh@gmail.com>
2025-12-25 21:13:00 +0700

    fix produk.php

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -6,9 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    protected $table = 'produk'; // TAMBAHKAN INI!
+    protected $table = 'produk'; 
     protected $fillable = ['nama', 'kategori_id', 'harga', 'gambar', 'is_available'];
 
-    public function komposisi() { return $this->hasMany(Komposisi::class); }
-    public function itemTransaksi() { return $this->hasMany(ItemTransaksi::class); }
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'kategori_id');
+    }
+
+    public function komposisi() 
+    { 
+        return $this->hasMany(Komposisi::class, 'produk_id'); 
+    }
+
+    public function itemTransaksi() 
+    { 
+        return $this->hasMany(ItemTransaksi::class, 'produk_id'); 
+    }
 }
